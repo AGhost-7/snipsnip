ec0f836ac649db59ff2c9db7e67ca82c4143fe55
AGhost-7 <jonathan.boudreau.92@gmail.com>
2019-01-11 01:51:53 +0000

    Fix issue on python 3 hosts

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='snipsnip',
-    version='0.4',
+    version='0.5',
     description='Remote mac clipboard',
     url='http://github.com/AGhost-7/snipsnip',
     author='Jonathan Boudreau',
