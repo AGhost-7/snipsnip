82c1ebfa3a10189536994d6b7764ea91e743b455
AGhost-7 <jonathan.boudreau.92@gmail.com>
2019-04-09 23:26:30 -0400

    add dependency for xlib

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
     keywords=['clipboard'],
     zip_safe=False,
     install_requires=[
-        'pyperclip >= 1.7.0'
+        'pyperclip >= 1.7.0',
+        'python-xlib >= 0.25;platform_system=="Linux"'
     ],
     entry_points={
         'console_scripts': ['snipsnip=snipsnip:main']
