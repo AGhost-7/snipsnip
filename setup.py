070ff811b9daeb1932b6bf0ca6fc8073bbdfa5bc
AGhost-7 <jonathan.boudreau.92@gmail.com>
2019-06-06 22:05:08 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='snipsnip',
-    version='0.5',
+    version='0.6',
     description='Remote mac clipboard',
     url='http://github.com/AGhost-7/snipsnip',
     author='Jonathan Boudreau',
