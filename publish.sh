802d2875f9c16f4c13574fac4050cd7c934de290
AGhost-7 <jonathan.boudreau.92@gmail.com>
2019-06-06 23:45:27 +0000

    finally works again...

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+rm -rf dist
+
 pip3 install --user --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='snipsnip',
-    version='0.7',
+    version='0.8',
     description='Remote mac clipboard',
     url='http://github.com/AGhost-7/snipsnip',
     author='Jonathan Boudreau',
